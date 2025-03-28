b4951ecf2d1774cc865c10646163ed78cdbed44c
Timax1278 <davydov.tymofiy.studente@itispaleocapa.it>
2025-03-28 08:42:33 +0000

    as

--- serenety/serenety-app/vue.config.js
+++ serenety/serenety-app/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    host: "0.0.0.0",  // Ascolta su tutte le interfacce di rete
+    port: 8080,        // Puoi usare una porta diversa se necessario
+    allowedHosts: "all",  // Permette tutte le richieste da host esterni, inclusi quelli di ngrok
+  },
 });
